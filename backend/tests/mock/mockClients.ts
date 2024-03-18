df90cd142de1832c3389472d1d0c65d0dd718a1d
reinaldoper <reinaldoper83@gmail.com>
2024-03-17 22:52:13 -0400

    refactor: ajusta mock backend e adicionado novo cliente

--- backend/tests/mock/mockClients.ts
+++ backend/tests/mock/mockClients.ts
@@ -2,10 +2,18 @@ export const clients = [
   {
     "id": 5,
     "nome": "fulana",
-    "status": "Inativo",
+    "status": "Aguardando ativação",
     "cpf": "429.282.070-82",
     "telefone": "(67) 99655-0243",
     "email": "fulana@test.com"
+  },
+  {
+    "id": 7,
+    "nome": "Seu novo valor",
+    "status": "Ativo",
+    "cpf": "885.515.330-70",
+    "telefone": "(22) 00000-1112",
+    "email": "email@email.com"
   },
   {
     "id": 1,
@@ -16,11 +24,11 @@ export const clients = [
     "email": "john_doe@test.com"
   },
   {
-    "id": 7,
-    "nome": "Seu novo valor",
-    "status": "Ativo",
-    "cpf": "885.515.330-70",
-    "telefone": "(22) 00000-0000",
-    "email": "email@email.com"
+    "id": 9,
+    "nome": "Joao Antonio",
+    "status": "Desativado",
+    "cpf": "856.101.690-62",
+    "telefone": "(65) 99999-0011",
+    "email": "jotaantonio@test.com"
   }
 ]
